8d0267a3300940718aa753e9e487b35cca4e1421
Mohamed Atwa <mohamedatwa50@hotmail.com>
2026-04-11 06:42:31 +0200

    Add gz resource path to launch file

--- lekiwi_description/launch/lekiwi_gz.launch.py
+++ lekiwi_description/launch/lekiwi_gz.launch.py
@@ -1,13 +1,13 @@
 #!/usr/bin env python3
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python import get_package_share_directory
 import os
-
+from pathlib import Path
 
 def generate_launch_description():
     lekiwi_description_dir = get_package_share_directory("lekiwi_description")
@@ -23,6 +23,13 @@ def generate_launch_description():
         executable="robot_state_publisher",
         parameters=[{"robot_description":robot_description}]
     )
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(lekiwi_description_dir).parent.resolve())
+            ]
+        )
+    
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
@@ -55,13 +62,13 @@ def generate_launch_description():
         output="screen",
         arguments=("-d",os.path.join(lekiwi_description_dir,"rviz", "display.rviz"))
     )
-
+    
     return LaunchDescription([
         model_arg,
+        gazebo_resource_path,
         robot_state_pub_node,
         gazebo,
         gz_spawn_entity,
         gz_bridge,
         rviz_node,
-        
     ])
